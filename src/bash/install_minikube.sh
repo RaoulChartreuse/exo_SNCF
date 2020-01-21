15e7470d5ef18241c31409a11ddf3288c50d9681
Laurent Morin <laurent.morin.morin@gmail.com>
2020-01-21 16:45:38 +0100

    update

--- src/bash/install_minikube.sh
+++ src/bash/install_minikube.sh
@@ -13,3 +13,4 @@ sudo apt-get update && sudo apt-get install docker.io -y
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x minikube
 sudo mv minikube /usr/local/bin/ 
+mkdir -p /var/lib/mysql
