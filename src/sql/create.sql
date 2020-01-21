321688081323ceae1634943e0d4bb00e4c7d67ad
laurent <laurent.morin.morin@gmail.com>
2020-01-21 09:48:11 +0100

    Add sql script

--- src/sql/create.sql
+++ src/sql/create.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE IF NOT EXIST sncf;
+CREATE DATABASE IF NOT EXISTS sncf;
+use sncf;
 
 CREATE TABLE IF NOT EXISTS gare (
     codegare int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
@@ -19,20 +20,34 @@ CREATE TABLE IF NOT EXISTS conducteur(
     codeconducteur int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     nom varchar(50) NOT NULL,
     prenom varchar(50),
-    secu varchar(13)
+    secu varchar(20)
 );
 
-
+/*
 CREATE TABLE IF NOT EXISTS trajet(
     codetrajet int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    garedepart int NOT NULL FOREIGN KEY references gare(codegare),
-    garearrivee int NOT NULL FOREIGN KEY references gare(codegare),
+    garedepart int FOREIGN KEY references gare(codegare),
+    garearrivee int FOREIGN KEY references gare(codegare),
     conducteur int NOT NULL FOREIGN KEY references conducteur(codeconducteur),
     rame int NOT NULL FOREIGN KEY references rame(coderame),
     depart datetime NOT NULL,
     duree TIME
-);
+);*/
+CREATE TABLE IF NOT EXISTS trajet(
+    codetrajet int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    garedepart int,
+    garearrivee int,
+    conducteur int,
+    rame int,
+    depart datetime NOT NULL,
+    duree TIME,
+    FOREIGN KEY (garedepart) REFERENCES gare(codegare),
+    FOREIGN KEY (garearrivee) REFERENCES gare(codegare),
+    FOREIGN KEY (conducteur) REFERENCES conducteur(codeconducteur),
+    FOREIGN KEY (rame) REFERENCES rame(coderame)
+);	  
 
-
+/*
 CREATE USER IF NOT EXIST 'user'@'%' IDENTIFIED BY 'user';
 GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';
+*/
---
+++ src/sql/insert.sql
@@ -0,0 +1,4 @@
+LOAD DATA INFILE '/tmp/dataset/conducteur.txt' INTO TABLE conducteur FIELDS TERMINATED BY ',';
+LOAD DATA INFILE '/tmp/dataset/gare.txt' INTO TABLE gare FIELDS TERMINATED BY ',';
+LOAD DATA INFILE '/tmp/dataset/rame.txt' INTO TABLE rame FIELDS TERMINATED BY ',';
+LOAD DATA INFILE '/tmp/dataset/trajet.txt' INTO TABLE trajet FIELDS TERMINATED BY ',';
