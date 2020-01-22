bc66dcb207f16eaeff466d94cab3c16129fe4280
laurent <laurent.morin.morin@gmail.com>
2020-01-22 16:52:44 +0100

     azeeazaze

--- src/bash/on_update.sh
+++ src/bash/on_update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#docker login -u laurentmorin --password-stdin <  ~/.dockpass
+docker login -u laurentmorin --password-stdin <  ~/.dockpass
 
 docker tag exo_sncf_httpdserver laurentmorin/exo_sncf_httpdserver:$(git log -n 1 --pretty=format:"%H")
-#docker push laurentmorin/exo_sncf_httpdserver:$(git log -n 1 --pretty=format:"%H")
+docker push laurentmorin/exo_sncf_httpdserver:$(git log -n 1 --pretty=format:"%H")
 
 docker tag exo_sncf_db laurentmorin/exo_sncf_db:$(git log -n 1 --pretty=format:"%H")
-#docker push laurentmorin/exo_sncf_db:$(git log -n 1 --pretty=format:"%H")
+docker push laurentmorin/exo_sncf_db:$(git log -n 1 --pretty=format:"%H")
 
 
 #docker pull laurentmorin/exo_sncf_db
--- src/bash/update_prod.sh
+++ src/bash/update_prod.sh
@@ -7,5 +7,5 @@ cd src/k8s/
 kustomize edit set image laurentmorin/exo_sncf_db:$(git log -n 1 --pretty=format:"%H")
 kustomize edit set image laurentmorin/exo_sncf_httpdserver:$(git log -n 1 --pretty=format:"%H")
 
-kustomize build . | sudo kubectl apply -f -
+echo laurent | sudo -S kustomize build . | sudo kubectl apply -f -
 cd ../..
