1f8c081eff7092df3c2f9f3a41d786a26103ea19
laurent <laurent.morin.morin@gmail.com>
2020-01-22 13:31:03 +0100

    V3

--- src/bash/on_update.sh
+++ src/bash/on_update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker login -u laurentmorin --password-stdin <  ~/.dockpass
 
-docker tag exo_sncf_httpdserver laurentmorin/exo_sncf_httpdserver:2
-docker push laurentmorin/exo_sncf_httpdserver:2
+docker tag exo_sncf_httpdserver laurentmorin/exo_sncf_httpdserver:3
+docker push laurentmorin/exo_sncf_httpdserver:3
 
-docker tag exo_sncf_db laurentmorin/exo_sncf_db:2
-docker push laurentmorin/exo_sncf_db:2
+docker tag exo_sncf_db laurentmorin/exo_sncf_db:3
+docker push laurentmorin/exo_sncf_db:3
 
 
 #docker pull laurentmorin/exo_sncf_db
